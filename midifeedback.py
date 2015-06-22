c917e6db4ff0c494db81c1acb77b5b86facdd1af
Sonejo <azvinz@yahoo.de>
2015-06-22 20:05:32 +0200

    Message

---
+++ midifeedback.py
@@ -0,0 +1,93 @@
+from queue import Queue, Empty
+import jack
+import time
+
+
+class midifeedback:
+
+    NOTEON = 0x9
+    NOTEOFF = 0x8
+    MIDICTRL = 11
+    NEW_CELL_STYLE = ("#cell_frame {border: 0px; "
+                      "border-radius: 5px; "
+                      "background-color: rgb(217, 217, 217);}")
+    NEW_CELL_STYLE_ROUND = ("#cell_frame {border: 0px; "
+                            "border-radius: 20px; "
+                            "background-color: rgb(217, 217, 217);}")
+
+    NOTE_NAME = ['C', 'C#',
+                 'D', 'D#',
+                 'E',
+                 'F', 'F#',
+                 'G', 'G#',
+                 'A', 'A#',
+                 'B']
+
+    def __init__(self):
+        self.queue_out = Queue()
+
+        self.client = jack.Client("SB Midi Feedback")
+        self.midi_out = self.client.midi_outports.register("output")
+        self.client.set_process_callback(self.my_callback)
+        self.client.activate()
+
+    def my_callback(self,frames):
+        self.midi_out.clear_buffer()
+        try:
+            i = 1
+            while True:
+                note = self.queue_out.get(block=False)
+                self.midi_out.write_midi_event(i, note)
+                i += 1
+        except Empty:
+            pass
+
+        return jack.CALL_AGAIN
+
+
+    def blackall(self):
+        for pitch in range(128):
+            note = ((self.NOTEON << 4) + 0, pitch, 12)
+            self.queue_out.put(note)
+
+
+    def closeall(self):
+        self.client.deactivate()
+        self.client.close()
+
+
+    def onenote(self, pitchx, pitchy):
+        pitch = pitchx + (pitchy*16)
+        color = 60
+        note = ((self.NOTEON << 4) + 0, pitch, color)
+        self.queue_out.put(note)
+
+
+
+
+    def lightFancy(self):
+        color = 60
+        fancylist = [19, 18, 33, 50, 67, 82, 81, 36, 37, 54, 69, 86, 101, 100, 84, 68, 52]
+
+        for pitch in fancylist:
+            note = ((self.NOTEON << 4) + 0, pitch, color)
+            self.queue_out.put(note)
+
+
+    def blink(self):
+        for pitch in range(128):
+            note = ((self.NOTEON << 4) + 0, pitch, 60)
+            self.queue_out.put(note)
+            time.sleep(0.5)
+            note = ((self.NOTEON << 4) + 0, pitch, 12)
+            self.queue_out.put(note)
+            time.sleep(0.5)
+
+
+#use:
+#import midifeedback
+#test = midifeedback.midifeedback()
+#test.lightFancy()
+#test.blackall()
+#test.closeall()
+#reload(midifeedback)
