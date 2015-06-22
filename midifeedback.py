13ed3dc347d4bacbdd2b393eac1669c04d54c134
Julian Jarecki <julian.jarecki@gmail.com>
2015-06-22 20:26:28 +0200

    moved color in argument ofonenoe method

--- midifeedback.py
+++ midifeedback.py
@@ -4,7 +4,6 @@ import time
 
 
 class midifeedback:
-
     NOTEON = 0x9
     NOTEOFF = 0x8
     MIDICTRL = 11
@@ -24,7 +23,7 @@ class midifeedback:
         self.client.set_process_callback(self.my_callback)
         self.client.activate()
 
-    def my_callback(self,frames):
+    def my_callback(self, frames):
         self.midi_out.clear_buffer()
         try:
             i = 1
@@ -37,35 +36,28 @@ class midifeedback:
 
         return jack.CALL_AGAIN
 
-
     def blackall(self):
         for pitch in range(128):
             note = ((self.NOTEON << 4) + 0, pitch, 12)
             self.queue_out.put(note)
 
-
     def closeall(self):
         self.client.deactivate()
         self.client.close()
 
-
-    def onenote(self, pitchx, pitchy):
-        pitch = pitchx + (pitchy*16)
-        color = 60
+    def onenote(self, pitchx, pitchy, color=60):
+        pitch = pitchx + (pitchy * 16)
         note = ((self.NOTEON << 4) + 0, pitch, color)
         self.queue_out.put(note)
 
-
-
-
     def lightFancy(self):
         color = 60
-        fancylist = [19, 18, 33, 50, 67, 82, 81, 36, 37, 54, 69, 86, 101, 100, 84, 68, 52]
+        fancylist = [19, 18, 33, 50, 67, 82, 81, 36, 37, 54, 69, 86, 101, 100,
+                     84, 68, 52]
 
         for pitch in fancylist:
             note = ((self.NOTEON << 4) + 0, pitch, color)
             self.queue_out.put(note)
-
 
     def blink(self):
         for pitch in range(128):
@@ -77,11 +69,11 @@ class midifeedback:
             time.sleep(0.1)
 
 
-#use:
-#import midifeedback
-#test = midifeedback.midifeedback()
-#test.lightFancy()
-#test.blackall()
-#test.closeall()
-#reload(midifeedback)
-#test.onenote(x,y)
+            # use:
+            # import midifeedback
+            # test = midifeedback.midifeedback()
+            # test.lightFancy()
+            # test.blackall()
+            # test.closeall()
+            # reload(midifeedback)
+            # test.onenote(x,y)
